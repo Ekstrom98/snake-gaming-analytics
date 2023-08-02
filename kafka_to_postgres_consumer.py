e950a87eef05cb524cd8b5c71a0609153e91a4d8
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-02 15:26:48 +0200

    added functionality for transferring data from the topic snake_head_positions

--- kafka_to_postgres_consumer.py
+++ kafka_to_postgres_consumer.py
@@ -14,12 +14,12 @@ PORT = config['POSTGRES']['PORT']
 
 bootstrap_server = config['KAFKA']['bootstrap_server']
 
-initialization_consumer = KafkaConsumer('initializations', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=6000)
-food_positions_consumer = KafkaConsumer('food_positions', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=6000)
+initialization_consumer = KafkaConsumer('initializations', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=1000)
+food_positions_consumer = KafkaConsumer('food_positions', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=1000)
 snake_head_positions_consumer = KafkaConsumer('snake_head_positions', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=6000)
-events_consumer = KafkaConsumer('events', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=6000)
-scores_consumer = KafkaConsumer('scores', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=6000)
-game_overs_consumer = KafkaConsumer('game_overs', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=5000)
+events_consumer = KafkaConsumer('events', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=1000)
+scores_consumer = KafkaConsumer('scores', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=1000)
+game_overs_consumer = KafkaConsumer('game_overs', bootstrap_servers=bootstrap_server, auto_offset_reset='earliest', consumer_timeout_ms=1000)
 
 try:
     connection = psycopg2.connect(
@@ -101,6 +101,32 @@ for msg in food_positions_consumer:
         # Data to be inserted 
         data_to_insert = (f'{game_id}', f'{food_x}', f'{food_y}', f'{time}')
 
+        insert_data(cursor, connection, insert_query, data_to_insert)
+
+for msg in snake_head_positions_consumer:
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
+
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
+
+        # Extracting the data
+        game_id = data_json['game_id']
+        head_x = int(data_json['head_x'])
+        head_y = int(data_json['head_y'])
+        time = data_json['time']
+        
+        # Convert Unix timestamp to a Python datetime object
+        time = datetime.utcfromtimestamp(time)
+        # Convert the datetime object to a string representation in PostgreSQL's timestamp format
+        time = time.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        # Define insert query
+        insert_query = "INSERT INTO snake_head_positions (game_id, head_x, head_y, time) VALUES (%s, %s, %s, %s)"
+
+        # Data to be inserted 
+        data_to_insert = (f'{game_id}', f'{head_x}', f'{head_y}', f'{time}')
+        
         insert_data(cursor, connection, insert_query, data_to_insert)
 
 
