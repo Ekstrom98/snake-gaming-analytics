0c988643f1fc9704daa73e2d8832e51cb4bb58d8
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-02 17:25:10 +0200

    script now only reads new data based on game_id.

--- kafka_to_postgres_consumer.py
+++ kafka_to_postgres_consumer.py
@@ -36,36 +36,63 @@ except Exception as e:
 
 cursor = connection.cursor()
 
-def insert_data(cursor, connection, insert_query, data_to_insert):
-    try:
-        cursor = connection.cursor()
+def check_if_game_id_exists(table, game_id):
+    cursor.execute(f"""
+    SELECT 
+        *
+        FROM       
+        {table}
+        WHERE game_id = '{game_id}'
+    """)
+    results = cursor.fetchall() 
+    nbr_matching_records = len(results)                       
+    
+    if (nbr_matching_records == 0):
+        return False
+    else:
+        return True
 
+def insert_data(insert_query, data_to_insert):
+    try:
         # Execute the INSERT query with the data
         cursor.execute(insert_query, data_to_insert)
 
-        # Commit the changes to the database
-        connection.commit()
     except Exception as e:
         print("Failed to insert data.")
         print("Error: " + str(e))
 try:
+    insert_values=True
+    i = 0
     for msg in initialization_consumer:
-            #print(msg.topic, msg.offset, type(msg))
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
+            
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Extracting the data
+        game_id = data_json['game_id']
+
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "initializations", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+
+        if insert_values == True:
             player = data_json['player']
             screen_width = data_json['screen_width']
             screen_height = data_json['screen_height']
             platform = data_json['platform']
             init_time = data_json['init_time']
+
             # Convert Unix timestamp to a Python datetime object
             init_time = datetime.utcfromtimestamp(init_time)
+
             # Convert the datetime object to a string representation in PostgreSQL's timestamp format
             init_time = init_time.strftime("%Y-%m-%d %H:%M:%S.%f")
 
@@ -74,23 +101,44 @@ try:
 
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{player}', f'{screen_width}', f'{screen_height}', f'{platform}', f'{init_time}')
-
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else:
+                pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic initializations transferred successfully.")
 except Exception as e:
     print("Data could not be transferred correctly from the topic initializations.")
     print("Error: " + str(e))
 
-try:  
+
+
+try:
+    insert_values = True  
+    i = 0
     for msg in food_positions_consumer:
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
+            
+            
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Extracting the data
+        game_id = data_json['game_id']
+
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "food_positions", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+
+        if insert_values == True:
             food_x = data_json['food_x']
             food_y = data_json['food_y']
             time = data_json['time']
@@ -106,22 +154,40 @@ try:
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{food_x}', f'{food_y}', f'{time}')
 
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else:
+            pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic food_positions transferred successfully.")
 except Exception as e:
      print("Data could not be transferred correctly from the topic food_positions.")
      print("Error: " + str(e))
 
-try: 
+try:
+    insert_values = True
+    i = 0
     for msg in snake_head_positions_consumer:
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
+
+        # Extracting the data
+        game_id = data_json['game_id']
+
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "snake_head_positions", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+        if insert_values == True:
             head_x = int(data_json['head_x'])
             head_y = int(data_json['head_y'])
             time = data_json['time']
@@ -137,22 +203,38 @@ try:
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{head_x}', f'{head_y}', f'{time}')
 
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else: 
+            pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic snake_head_positions transferred successfully.")
 except Exception as e:
      print("Data could not be transferred correctly from the topic snake_head_positions.")
      print("Error: " + str(e))
 
 try:
+    insert_values = True
+    i = 0
     for msg in events_consumer:
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Extracting the data
+        game_id = data_json['game_id']
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "events", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+        if insert_values == True:
             event_key = data_json['event_key']
             time = data_json['time']
             
@@ -167,7 +249,12 @@ try:
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{event_key}', f'{time}')
             
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else:
+            pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic events transferred successfully.")
 except Exception as e:
      print("Data could not be transferred correctly from the topic events.")
@@ -175,15 +262,26 @@ except Exception as e:
 
 
 try:
+    insert_values = True
+    i = 0
     for msg in scores_consumer:
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Extracting the data
+        game_id = data_json['game_id']
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "scores", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+        if insert_values == True:
             score = data_json['score']
             time = data_json['time']
             
@@ -198,22 +296,38 @@ try:
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{score}', f'{time}')
             
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else:
+            pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic scores transferred successfully.")
 except Exception as e:
      print("Data could not be transferred correctly from the topic scores.")
      print("Error: " + str(e))
 
 try:
+    insert_values = True
+    i = 0
     for msg in game_overs_consumer:
-            # Decoding the bytes to a string
-            data_string = msg.value.decode('utf-8')
+        # Decoding the bytes to a string
+        data_string = msg.value.decode('utf-8')
 
-            # Parsing the JSON string
-            data_json = json.loads(data_string)
+        # Parsing the JSON string
+        data_json = json.loads(data_string)
 
-            # Extracting the data
-            game_id = data_json['game_id']
+        # Extracting the data
+        game_id = data_json['game_id']
+        if i == 0:
+            game_id_already_inserted = check_if_game_id_exists(table = "game_overs", game_id = game_id)
+            if game_id_already_inserted == True:
+                insert_values = False
+            else:
+                pass
+        else: 
+            pass
+        if insert_values == True:
             collision_type = data_json['collision_type']
             score = data_json['score']
             time = data_json['time']
@@ -229,7 +343,12 @@ try:
             # Data to be inserted 
             data_to_insert = (f'{game_id}', f'{collision_type}', f'{score}', f'{time}')
             
-            insert_data(cursor, connection, insert_query, data_to_insert)
+            insert_data(insert_query, data_to_insert)
+        else:
+            pass
+        i += 1
+    # Commit the changes to the database
+    connection.commit()
     print("Data from the topic game_overs transferred successfully.")
 except Exception as e:
      print("Data could not be transferred correctly from the topic game_overs.")
