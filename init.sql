e2c00eb451b9e8f83e076f18e91523d06672c25b
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-02 16:59:12 +0200

    added index on game_id for all tables

--- init.sql
+++ init.sql
@@ -7,6 +7,7 @@ screen_height smallint,
 platform varchar(7),
 init_time timestamp
 );
+CREATE INDEX idx_initializations_game_id ON initializations (game_id);
 
 CREATE TABLE IF NOT EXISTS food_positions (
 id SERIAL PRIMARY KEY,
@@ -15,6 +16,7 @@ food_x smallint,
 food_y smallint,
 time timestamp
 );
+CREATE INDEX idx_food_positions_game_id ON food_positions (game_id);
 
 CREATE TABLE IF NOT EXISTS snake_head_positions (
 id SERIAL PRIMARY KEY,
@@ -23,6 +25,7 @@ head_x smallint,
 head_y smallint,
 time timestamp
 );
+CREATE INDEX idx_snake_head_positions_game_id ON snake_head_positions (game_id);
 
 CREATE TABLE IF NOT EXISTS events (
 id SERIAL PRIMARY KEY,
@@ -30,6 +33,7 @@ game_id varchar(64),
 event_key int,
 time timestamp
 );
+CREATE INDEX idx_events_game_id ON events (game_id);
 
 CREATE TABLE IF NOT EXISTS scores (
 id SERIAL PRIMARY KEY,
@@ -37,6 +41,7 @@ game_id varchar(64),
 score smallint,
 time timestamp
 );
+CREATE INDEX idx_scores_game_id ON scores (game_id);
 
 CREATE TABLE IF NOT EXISTS game_overs (
 id SERIAL PRIMARY KEY,
@@ -45,3 +50,4 @@ collision_type varchar(8),
 score smallint,
 time timestamp
 );
+CREATE INDEX idx_game_overs_game_id ON game_overs (game_id);
