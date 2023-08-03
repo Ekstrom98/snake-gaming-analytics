7f1382085b9337ba2add7c000fe5b15468330862
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-03 17:19:00 +0200

    added sql script for fetching all games

---
+++ sql/get_all_games.sql
@@ -0,0 +1,5 @@
+SELECT i.player, "go".score, "go".collision_type, TO_CHAR("go".time , 'YYYY-MM-DD HH24:MI:SS')
+FROM game_overs go
+JOIN initializations i
+ON "go".game_id = i.game_id
+ORDER BY time DESC;
