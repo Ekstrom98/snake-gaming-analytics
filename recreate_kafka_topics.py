253acf7ab25f57e6b581407388cec648cd6b0d05
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-04 17:09:17 +0200

    read bootstrap server from config file for consistency

--- recreate_kafka_topics.py
+++ recreate_kafka_topics.py
@@ -1,18 +1,22 @@
-import subprocess
+import subprocess, configparser
 
-topics = subprocess.run(['kafka-topics', '--list', '--bootstrap-server', 'localhost:9092'], stdout=subprocess.PIPE)\
+config = configparser.ConfigParser()
+config.read('config.cfg')
+bootstrap_server = config['KAFKA']['BOOTSTRAP_SERVER']
+
+topics = subprocess.run(['kafka-topics', '--list', '--bootstrap-server', f'{bootstrap_server}'], stdout=subprocess.PIPE)\
                    .stdout.decode('utf-8').strip().split('\n')
 
 
 # Delete all topics
 print("Starting deletion of Kafka topics...")
 for topic in topics:
-    subprocess.run(['kafka-topics', '--delete', '--topic', f'{topic}', '--bootstrap-server', 'localhost:9092'])
+    subprocess.run(['kafka-topics', '--delete', '--topic', f'{topic}', '--bootstrap-server', f'{bootstrap_server}'])
 print("All topics have been deleted.")
 
 # Create all topics
 print("Starting creation of Kafka topics...")
 for topic in topics:
-    subprocess.run(['kafka-topics', '--create', '--topic', f'{topic}', '--bootstrap-server', 'localhost:9092',
+    subprocess.run(['kafka-topics', '--create', '--topic', f'{topic}', '--bootstrap-server', f'{bootstrap_server}',
                     '--partitions', '1', 'replication-factor', '1'])
 print("All topics have been created.")
