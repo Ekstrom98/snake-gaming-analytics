6b217a2313f96d384dc87cf5c8c543d4bae8d169
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-04 12:33:49 +0200

    added feature so that updated data is automatically pushed to my repo

--- query_database.py
+++ query_database.py
@@ -1,21 +1,20 @@
 from database import database
 import csv
+import subprocess
 
 db = database()
 db.connect(return_connection=False)
 
-# Execute script for finding the top 3 best games
+# Execute script for finding the top 3 best games and write to a csv file
 top_3_results = db.execute_sql_script("./sql/top_3.sql")
-# Open a file in write mode
 with open('./query_results/top_3_results.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     # Write the results to the CSV file
     for result in top_3_results:
         writer.writerow(result)
 
-# Execute script for finding the top 3 best games
+# Execute script fetching all games that have been played and write to a csv file
 get_all_games = db.execute_sql_script("./sql/get_all_games.sql")
-# Open a file in write mode
 with open('./query_results/get_all_games.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     # Write the results to the CSV file
@@ -24,3 +23,11 @@ with open('./query_results/get_all_games.csv', 'w', newline='') as file:
 
 db.disconnect()
 
+# Adding the specified folder
+subprocess.run(['git', 'add', 'query_results'])
+
+# Committing with the specified message
+subprocess.run(['git', 'commit', '-m', 'updated data'])
+
+# Pushing the changes
+subprocess.run(['git', 'push'])
