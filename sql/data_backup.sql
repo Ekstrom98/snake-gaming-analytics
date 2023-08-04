562ddbc9733f06437acb48262c796c26774568f3
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-04 15:12:13 +0200

    psql for copying all data

---
+++ sql/data_backup.sql
@@ -0,0 +1,6 @@
+\COPY events TO '{path}' WITH CSV;
+\COPY food_positions TO '{path}' WITH CSV;
+\COPY game_overs TO '{path}' WITH CSV;
+\COPY initializations TO '{path}' WITH CSV;
+\COPY scores TO '{path}' WITH CSV;
+\COPY snake_head_positions TO '{path}' WITH CSV;
