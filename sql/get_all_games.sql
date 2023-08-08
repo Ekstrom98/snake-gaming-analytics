bf70e77638ea3f6fdc50886d211de960712d37c0
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-08 15:26:17 +0200

    minor modification

--- sql/get_all_games.sql
+++ sql/get_all_games.sql
@@ -1,5 +1,5 @@
 SELECT i.player, "go".score, "go".collision_type, TO_CHAR("go".time , 'YYYY-MM-DD HH24:MI:SS')
-FROM game_overs go
+FROM game_overs "go"
 JOIN initializations i
 ON "go".game_id = i.game_id
 ORDER BY "go".score DESC;
