d732cd2ff5acffa1519268fdd29a8b207a657bcb
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-08-08 15:38:52 +0200

    added duration

--- sql/get_all_games.sql
+++ sql/get_all_games.sql
@@ -1,4 +1,4 @@
-SELECT i.player, "go".score, "go".collision_type, TO_CHAR("go".time , 'YYYY-MM-DD HH24:MI:SS')
+SELECT i.player, "go".score, "go".collision_type, "go".time-i.init_time time_played, TO_CHAR("go".time , 'YYYY-MM-DD HH24:MI:SS')
 FROM game_overs "go"
 JOIN initializations i
 ON "go".game_id = i.game_id
